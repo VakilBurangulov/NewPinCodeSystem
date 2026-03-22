6c7795da9eca464fbfd247dab879392817fba15c
Vakil <burangulov.vakil@gmail.com>
2026-03-22 19:54:31 +0300

    Добавьте файлы проекта.

---
+++ NewPinCodeSystem/NewPinCodeSystem.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    int sum = 0;
+    {
+        int count = 1;
+        int num;
+
+        while (count < 5) {
+            std::cout << "Введите " << count << "ое число: ";
+            std::cin >> num;
+            sum += num;
+            count++;
+        }
+    }
+
+    if (sum == 42) {
+        std::cout << "Ввод корректный\n";
+    }
+    else {
+        std::cout << "Ввод некорректный\n";
+    }
+}
